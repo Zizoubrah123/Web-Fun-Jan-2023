338d4fb061fc0a4ae577be7b2c8a44671ae4345a
aziz <johndoe@example.com>
2023-05-08 15:01:12 +0200

    uwu

---
+++ algorith.js
@@ -0,0 +1,18 @@
+var arr = ["a","b","c","d","e"]
+
+var temp = ["e","d","c","b","a"]
+var arr = temp
+
+console.log(arr);
+
+
+
+// function reverse(arr){
+//     var imv = []
+//     for (var i = array.length-1; i>= 0; i--)
+//     imv.push(array[i])
+// }
+// var arr = ["1","2","3","4"]
+// var result= reverse(arr)
+// console.log(result)
+
