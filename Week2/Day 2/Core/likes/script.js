6b63f76829c840be442f2c11c12b0e6dfa934680
aziz <johndoe@example.com>
2023-05-11 23:34:01 +0200

    ss

---
+++ Week2/Day 2/Core/likes/script.js
@@ -0,0 +1,18 @@
+function increment() {
+    const numberElement = document.getElementById("ps1");
+    let number = parseInt(numberElement.innerText);
+    number++;
+    numberElement.innerText = number;
+}
+function increment1() {
+    const numberElement = document.getElementById("ps2");
+    let number = parseInt(numberElement.innerText);
+    number++;
+    numberElement.innerText = number;
+}
+function increment2() {
+    const numberElement = document.getElementById("ps3");
+    let number = parseInt(numberElement.innerText);
+    number++;
+    numberElement.innerText = number;
+}
