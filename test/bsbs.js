3632c4b929ce194dfc098ecc1cfd0d5b51fd4b22
aziz <johndoe@example.com>
2023-05-11 20:53:45 +0200

    zezer

--- test/bsbs.js
+++ test/bsbs.js
@@ -12,18 +12,34 @@
 // }
  
 
-function over(element) {
-    alert("mouseover");    
-}
+// function over(element) {
+//     alert("mouseover");    
+// }
     
-function out(element) {
-    alert("mouseout");    
+// function out(element) {
+//     alert("mouseout");    
+// }
+
+// function over(element) {
+//     element.style.backgroundColor = "lime";    
+// }
+    
+// function out(element) {
+//     element.style.backgroundColor = "silver";   
+// }
+// ---------------------------------------------------------
+function increment() {
+  const numberElement = document.getElementById("psspss");
+  let number = parseInt(numberElement.innerText);
+  number++;
+  numberElement.innerText = number;
 }
 
-function over(element) {
-    element.style.backgroundColor = "lime";    
-}
-    
-function out(element) {
-    element.style.backgroundColor = "silver";   
-}
+var h3 = document.querySelector("h3");
+h3.innerText = "New Title";
+
+
+
+
+
+
