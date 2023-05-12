3630736f301103407a5f8f4ee374ca839a44b237
aziz <johndoe@example.com>
2023-05-12 20:19:17 +0200

    sss

---
+++ Week2/Day 2/Practice/pizzaria.js
@@ -0,0 +1,49 @@
+function pizzaOven(psps, bsbs, rrr, aaaa ) {
+    var s1 = {};
+    s1.key1 = psps
+    s1.trad = bsbs
+    s1.mozza = rrr
+    s1.peppe = aaaa
+return s1;
+}
+
+var pizza = pizzaOven("deep dish", "traditional", ["mozzarella"], ["pepperoni", "sausage"])
+console.log(pizza)
+
+function pizzaOven(psps, bsbs, rrr, aaaa ) {
+    var s1 = {};
+    s1.key1 = psps
+    s1.key2 = bsbs
+    s1.key3 = rrr
+    s1.key4 = aaaa
+return s1;
+}
+
+var pizza = pizzaOven("hand tossed", "marinara", ["mozzarella", "feta"], ["mashrooms", "olives", "onions"])
+console.log(pizza)
+
+// my pizzas
+
+function pizzaOven(psps, bsbs, rrr, aaaa ) {
+    var s1 = {};
+    s1.key1 = psps
+    s1.trad = bsbs
+    s1.mozza = rrr
+    s1.peppe = aaaa
+return s1;
+}
+
+var pizza = pizzaOven("thon", "scalope", ["mozzarella"], ["pepperoni", "sausage"])
+console.log(pizza)
+
+function pizzaOven(psps, bsbs, rrr, aaaa ) {
+    var s1 = {};
+    s1.key1 = psps
+    s1.key2 = bsbs
+    s1.key3 = rrr
+    s1.key4 = aaaa
+return s1;
+}
+
+var pizza = pizzaOven("pepperoni", "sbaghetti", ["mozzarella", "feta"], ["mashrooms", "olives", "onions"])
+console.log(pizza)
